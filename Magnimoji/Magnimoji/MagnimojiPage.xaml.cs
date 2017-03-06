744fd9b3c054e698fdfd441cf5467aae848f7be7
Steve Hall <steve@SteveMacBookPro.local>
2017-03-06 10:50:37 -0300

    Initial Commit

---
+++ Magnimoji/Magnimoji/MagnimojiPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace Magnimoji
+{
+	public partial class MagnimojiPage : ContentPage
+	{
+		public MagnimojiPage()
+		{
+			InitializeComponent();
+
+			MyEntry.TextChanged += MyEntry_TextChanged;;
+		}
+
+		void MyEntry_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
+			if (MyEntry.Text.Length > 0)
+			{
+				emoji.Text = MyEntry.Text;
+				MyEntry.Text = "";
+			}
+		}
+	}
+}
